be27699e2ce53180ebc37357b2fc938fba3219db
Ubuntu <azureuser@ubuntu-1.m45n3boktoiejjgqygxyooi4eh.rx.internal.cloudapp.net>
2024-12-18 08:17:15 +0000

    commit

--- src/main/java/com/bosch/stocktoship/StocktoshipApplication.java
+++ src/main/java/com/bosch/stocktoship/StocktoshipApplication.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 
 import com.bosch.stocktoship.exception.InvalidIntegerException;
 import com.bosch.stocktoship.exception.InvalidItemCodeException;
@@ -12,10 +15,15 @@ import com.bosch.stocktoship.exception.InvalidItemCodeException;
 
 
 @SpringBootApplication
-public class StocktoshipApplication {
+public class StocktoshipApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StocktoshipApplication.class, args);
 	}
+
+	@Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        	return application.sources(StocktoshipApplication.class);
+    	}
 
 }
